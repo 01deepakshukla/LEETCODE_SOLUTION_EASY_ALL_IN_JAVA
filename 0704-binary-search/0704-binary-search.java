9f8db7ec62da0b6502c9eb0ecad5c47556bf8c29
Deepak Shukla <deepakshukla3280@gmail.com>
2026-06-26 17:41:10 +0530

    Time: 0 ms (100%), Space: 48.4 MB (49.43%) - LeetHub

--- 0704-binary-search/0704-binary-search.java
+++ 0704-binary-search/0704-binary-search.java
@@ -1,23 +1,21 @@
- class Solution {
-    public static int search(int arr[] , int target){
+class Solution {
+    public static int search(int arr[],int target){
         int start = 0;
         int end = arr.length-1;
-        while(start<=end){
+        while(start <= end){
             int mid = start+(end- start)/2;
-            if(arr[mid]==target){
+            if(arr[mid]== target){
                 return mid;
             }
             if(arr[mid]<target){
                 start = mid+1;
             }else{
-                end=mid-1;
+                end = mid-1;
             }
         }
         return -1;
     }
- }
-        
-        
- 
- 
+            }
+
+
  
